50c4bd5161f829e3a394af83cc598cdb68c9e272
bneilson42 <bneilson42@ymail.com>
2017-04-09 15:04:07 -0700

    Adding user profile and user test specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "Invalid user information"
       redirect_to edit_user_registration_path
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
+    @comments = @user.comments
   end
 
   private
